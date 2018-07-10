6cbab3140154710af4aa26afc9340895af2633b9
Nelson Junior <cazukyjunior@gmail.com>
2018-07-10 06:31:30 -0500

    Set banner content

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var header = require('gulp-header');
 var connect = require('gulp-connect');
 var imagemin = require('gulp-imagemin');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Tingleware Developers - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright ' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license %> (https://github.com/nelsonmfinda/<%= pkg.name %>/blob/master/LICENSE)\n',
+    ' */\n',
+    ''
+].join('');
 
 //style paths
 var ficheirosSass = 'src/sass/**/*.scss',
@@ -18,6 +29,9 @@ gulp.task('webserver', function () {
 gulp.task('styles', function () {
     gulp.src(ficheirosSass)
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
         .pipe(gulp.dest(destino))
         .pipe(connect.reload());
 });
